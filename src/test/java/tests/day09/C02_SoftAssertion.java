1a323bb6aac8abdd6cb23b73bc32b0358eaf4b87
fg2406 <flzgltr11@gmail.com>
2022-01-27 13:24:39 +0100

    practice

--- src/test/java/tests/day09/C02_SoftAssertion.java
+++ src/test/java/tests/day09/C02_SoftAssertion.java
@@ -35,32 +35,41 @@ import java.util.List;
     public void test01(){
       // 1. “http://zero.webappsecurity.com/” Adresine gidin
       driver.get("http://zero.webappsecurity.com/");
+
       // 2. Sign in butonuna basin
       driver.findElement(By.xpath("//button[@id='signin_button']")).click();
+
       // 3. Login kutusuna “username” yazin
       WebElement loginKutusu=driver.findElement(By.xpath("//input[@id='user_login']"));
       loginKutusu.sendKeys("username");
+
       // 4. Password kutusuna “password” yazin
       WebElement passwordKutusu=driver.findElement(By.xpath("//input[@id='user_password']"));
       passwordKutusu.sendKeys("password");
+
       // 5. Sign in tusuna basin
       driver.findElement(By.xpath("//input[@value='Sign in']")).click();
+
       // 6. Online banking menusu altinda Pay Bills sayfasina gidin
       driver.navigate().back(); // guvenli degil uyarisini geciyor
       driver.findElement(By.xpath("//strong[text( )='Online Banking']")).click();
       driver.findElement(By.xpath("//span[@id='pay_bills_link']")).click();
+
       // 7. “Purchase Foreign Currency” tusuna basin
       driver.findElement(By.xpath("//a[text()='Purchase Foreign Currency']")).click();
+
       // 8. “Currency” drop down menusunden Eurozone’u secin
       WebElement dropdown=driver.findElement(By.xpath("//select[@id='pc_currency']"));
       Select select=new Select(dropdown);
       select.selectByValue("EUR");
+
       // 9. soft assert kullanarak "Eurozone (Euro)" secildigini test edin
       SoftAssert softAssert = new SoftAssert();
       String actualSecim=select.getFirstSelectedOption().getText();
       String expectedSecim="Eurozone (euro)";
       softAssert.assertEquals(actualSecim,expectedSecim,"Dropdown dogru secilemedi");
       System.out.println("Hard assertion'da ilk assertion failed olsaydi, bu satir calismazdi");
+
       // 10. soft assert kullanarak DropDown listesinin su secenekleri oldugunu test edin "Select One", "Australia (dollar)", "Canada (dollar)", "Switzerland (franc)", "China (yuan)", "Denmark (krone)", "Eurozone (euro)", "Great Britain (pound)", "Hong Kong (dollar)", "Japan (yen)", "Mexico (peso)", "Norway (krone)", "New Zealand (dollar)", "Sweden (krona)", "Singapore (dollar)", "Thailand (baht)"
       List<WebElement> optionList= select.getOptions();
       String actualOptionListesiStringOlarak="";
--- src/test/java/tests/practice/C02.java
+++ src/test/java/tests/practice/C02.java
@@ -1,2 +1,20 @@
-package tests.practice;public class C02 {
+package tests.practice;
+
+public class C02 {
+
+ /*
+...Exercise2...
+   http://www.bestbuy.com 'a gidin,
+   Sayfa basliginin "Best" icerdigini(contains) dogrulayin
+   Ayrica Relative Locator kullanarak;
+       logoTest => BestBuy logosunun gorunutulenip goruntulenmedigini dogrulayin
+       mexicoLinkTest => Linkin gorunutulenip goruntulenmedigini dogrulayin
+
+*/
+
+
+
+
+
+
 }
